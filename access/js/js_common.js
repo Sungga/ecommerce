d89f8a6d2b842fe66788fb605f1d15342caeb92b
vietanh23102003 <anhngo2033@gmail.com>
2024-07-17 10:09:01 +0700

    vanh

--- access/js/js_common.js
+++ access/js/js_common.js
@@ -1,6 +1,10 @@
 // --------------------<< SHOW HISTORY SEARCH WHEN USER FOCUS TO SEARCH SEARCH >>----------------
 let searchBtn = document.querySelector('.header__header-bottom--center input');
 let historySearch = document.querySelector('.search-history');
+let menu = document.querySelector('.menu__box');
+let openmenu = document.querySelector('.icon_menu');
+let exitmenu = document.querySelector('.icon_exit');
+let search = document.querySelector('.icon_search');
 
 searchBtn.addEventListener('focus', function() {
     historySearch.style.display = 'block';
@@ -8,4 +12,19 @@ searchBtn.addEventListener('focus', function() {
 
 searchBtn.addEventListener('blur', function() {
     historySearch.style.display = 'none';
+})
+
+// --------------------<< SHOW MENU >>----------------
+openmenu.addEventListener('click', function(){
+    menu.style.display = 'block';
+})
+
+// --------------------<< EXIT MENU >>----------------
+exitmenu.addEventListener('click', function(){
+    menu.style.display = 'none';
+})
+
+// --------------------<< OPEN SEARCH >>----------------
+search.addEventListener('click', function(){
+    searchBtn.style.display = 'block';
 })
